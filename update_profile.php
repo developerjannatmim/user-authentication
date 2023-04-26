d54b2b8ef787a29758add0ee50471c1d74b55997
Jannatul Ferdous Mim <127183567+developerjannatmim@users.noreply.github.com>
2023-04-26 16:30:52 +0600

    updated

--- update_profile.php
+++ update_profile.php
@@ -1,52 +1,116 @@
 <?php
 session_start();
-$conn = new mysqli('localhost','root','','userauthentication');
+$conn = new mysqli('sql206.epizy.com','epiz_33834643','LnPlh0qbAvne','epiz_33834643_userauthentication');
 if(!$conn){
     die("Database Connection Failed");
 }
 
 $user_id = $_SESSION['user_id'];
 
+$sql = "SELECT * FROM users WHERE user_id = '$user_id'";
+$select = mysqli_query($conn, $sql) or die('query failed');
+if($select->num_rows > 0){
+  $fetch = mysqli_fetch_assoc($select);
+}
+
+
+
 if(isset($_POST['update_profile'])){
 
-   $update_name = mysqli_real_escape_string($conn, $_POST['update_name']);
-   $update_email = mysqli_real_escape_string($conn, $_POST['update_email']);
 
-   mysqli_query($conn, "UPDATE `users` SET user_name = '$update_name', user_email = '$update_email' WHERE user_id  = '$user_id'") or die('query failed');
-
-   $old_pass = mysqli_real_escape_string($conn,$_POST['old_pass']);
-   $update_pass = mysqli_real_escape_string($conn, md5($_POST['update_pass']));
-   $new_pass = mysqli_real_escape_string($conn, md5($_POST['new_pass']));
-   $confirm_pass = mysqli_real_escape_string($conn, md5($_POST['confirm_pass']));
-
-   if(!empty($update_pass) || !empty($new_pass) || !empty($confirm_pass)){
-      if($update_pass != $old_pass){
-         $message[] = 'old password not matched!';
-      }elseif($new_pass != $confirm_pass){
-         $message[] = 'confirm password not matched!';
-      }else{
-         mysqli_query($conn, "UPDATE `users` SET user_password = '$confirm_pass' WHERE user_id = '$user_id'") or die('query failed');
-         $message[] = 'password updated successfully!';
-      }
+   $fields = [];
+   $errorMessages = [];
+   $successMessage = '';
+ 
+   $selectSql = "SELECT * FROM users WHERE user_id = '$user_id'";
+   $selectResult = mysqli_query($conn, $selectSql);
+   if (mysqli_num_rows($selectResult) > 0) {
+     $fetch = mysqli_fetch_assoc($selectResult);
    }
-
-   $update_image = $_FILES['update_image']['name'];
-   $update_image_size = $_FILES['update_image']['size'];
-   $update_image_tmp_name = $_FILES['update_image']['tmp_name'];
-   $update_image_folder = 'uploaded_img/'.$update_image;
-
-   if(!empty($update_image)){
-      if($update_image_size > 2000000){
-         $message[] = 'image is too large';
-      }else{
-         $image_update_query = mysqli_query($conn, "UPDATE `users` SET user_image = '$update_image' WHERE user_id = '$user_id'") or die('query failed');
-         if($image_update_query){
-            move_uploaded_file($update_image_tmp_name, $update_image_folder);
-         }
-         $message[] = 'image updated succssfully!';
-      }
+ 
+   $updateName = mysqli_real_escape_string($conn, $_POST['update_name']);
+   $updateEmail = mysqli_real_escape_string($conn, $_POST['update_email']);
+   $oldPassword = mysqli_real_escape_string($conn, $_POST['old_password']);
+   $newPassword = mysqli_real_escape_string($conn, $_POST['new_password']);
+   $confirmPassword = mysqli_real_escape_string($conn, $_POST['confirm_password']);
+   $updateImage = $_FILES['update_image'];
+ 
+   if (empty($updateName)) {
+     $errorMessages[] = 'Name is required.';
    }
+   elseif ($updateName == $fetch['user_name']) {
+   }
+   else {
+     $fields['user_name'] = $updateName;
+   }
+ 
+   if (empty($updateEmail)) {
+     $errorMessages[] = 'Email is required.';
+   }
+   elseif ($updateEmail == $fetch['user_email']) {
+   }
+   else {
+     $fields['user_email'] = $updateEmail;
+   }
+ 
+   if (empty($oldPassword)) {
+     $errorMessages[] = 'Old password is required.';
+   }
+   elseif ($fetch['user_password'] != md5($oldPassword)) {
+     $errorMessages[] = 'Old password does not match.';
+   }
+   elseif (empty($newPassword)) {
+     $errorMessages[] = 'New password is required.';
+   }
+   elseif (empty($confirmPassword)) {
+     $errorMessages[] = 'Confirm password is required.';
+   }
+   elseif ($newPassword != $confirmPassword) {
+     $errorMessages[] = 'New password and comfirm password do not match.';
+   }
+   else {
+     $fields['user_password'] = md5($newPassword);
+   }
+ 
+   if (empty($updateImage)) {
+     $errorMessages[] = 'Image is required.';
+   }
+   elseif ($updateImage['error'] == 4) {
+   }
+   elseif ($updateImage['size'] > 2000000) {
+     $errorMessages[] = 'Image is too large.';
+   }
+   else {
+     $updateImagePath = 'uploaded_img/' . $updateImage['name'];
+     move_uploaded_file($updateImage['tmp_name'], $updateImagePath);
+     $fields['user_image'] = $updateImagePath;
+   }
+ 
+   if (!empty($fields) && empty($errorMessages)) {
+     $updateSql = "UPDATE `users` SET ";
+     foreach($fields as $key => $value) {
+       $updateSql .= "$key = '$value'";
+ 
+       if ($key != array_key_last($fields)) {
+         $updateSql .= ", ";
+       }
+     }
+     $updateSql .= " WHERE user_id = $user_id";
+ 
+     mysqli_query($conn, $updateSql);
+     if (mysqli_affected_rows($conn)) {
+       $successMessage = 'Updated successfully.';
+     }
+   }
+ }
+ 
+ $selectSql = "SELECT * FROM users WHERE user_id = '$user_id'";
+ $selectResult = mysqli_query($conn, $selectSql);
+ if (mysqli_num_rows($selectResult) > 0) {
+   $fetch = mysqli_fetch_assoc($selectResult);
+ 
 
+   
 }
 
 ?>
@@ -81,12 +145,17 @@ if(isset($_POST['update_profile'])){
          if($fetch['user_image'] == ''){
             echo '<img src="images/default-avatar.png">';
          }else{
-            echo '<img src="uploaded_img/'.$fetch['user_image'].'">';
+            echo '<img src="'.$fetch['user_image'].'">';
          }
-         if(isset($message)){
-            foreach($message as $message){
-               echo '<div class="message">'.$message.'</div>';
+         
+         if (isset($errorMessages)) {
+            foreach($errorMessages as $errorMessage){
+               echo '<div class="message">' . $errorMessage . '</div>';
             }
+         }
+
+         if (isset($successMessage)) {
+           echo '<div class="message">' . $successMessage . '</div>';
          }
       ?>
             <div class="flex">
@@ -100,13 +169,12 @@ if(isset($_POST['update_profile'])){
                         accept="image/jpg, image/jpeg, image/png" class="box" />
                 </div>
                 <div class="inputBox">
-                    <input type="hidden" name="old_pass" value="<?php echo $fetch['user_password']; ?>" />
                     <span>old password :</span>
-                    <input type="password" name="update_pass" placeholder="enter previous password" class="box" />
+                    <input type="password" name="old_password" placeholder="enter old password" class="box" />
                     <span>new password :</span>
-                    <input type="password" name="new_pass" placeholder="enter new password" class="box" />
+                    <input type="password" name="new_password" placeholder="enter new password" class="box" />
                     <span>confirm password :</span>
-                    <input type="password" name="confirm_pass" placeholder="confirm new password" class="box" />
+                    <input type="password" name="confirm_password" placeholder="confirm new password" class="box" />
                 </div>
             </div>
             <input type="submit" value="update profile" name="update_profile" class="btn" />
